74e6e016732fecc82526bd472f2bd2ea1fe96f6e
laur80 <laur80raul@gmail.com>
2020-11-01 13:39:20 +0200

    Updates

---
+++ precache-manifest.7106195135847024eb0d325f84d9c2e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "049122b600170b118fd6",
+    "url": "/lights-out-game/static/css/main.d1e24abb.chunk.css"
+  },
+  {
+    "revision": "049122b600170b118fd6",
+    "url": "/lights-out-game/static/js/main.049122b6.chunk.js"
+  },
+  {
+    "revision": "0189dccc15bf4706c318",
+    "url": "/lights-out-game/static/js/1.0189dccc.chunk.js"
+  },
+  {
+    "revision": "c21eb99d7eac6d163be2",
+    "url": "/lights-out-game/static/js/runtime~main.c21eb99d.js"
+  },
+  {
+    "revision": "64ebb5456aae45cce62dc44768c25509",
+    "url": "/lights-out-game/index.html"
+  }
+];
